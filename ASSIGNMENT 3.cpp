00a05859e627927e01a547fb9a21cca27f2e0f88
DIVYA <116086452+loveforoneforever@users.noreply.github.com>
2024-05-27 13:20:44 +0530

    Add files via upload

---
+++ ASSIGNMENT 3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main( ){
+	int age;
+	cout <<" Enter your age: ";
+	cin >> age;
+	
+	if (age >=18){
+		cout << "YOU ARE ELIGIBLE." <<endl;
+	} else {
+		cout << "YOU ARE NOT ELIGIBLE." << endl;
+	}
+  return 0;
+	}
+
